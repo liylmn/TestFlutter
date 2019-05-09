0c0ed3bbaf9b87287cea8ff771a3774279c2e7cd
李阳 <liyang2017@sinosoft.com.cn>
2019-05-09 18:06:13 +0800

    ceshi

--- app/src/main/java/com/sinosoft/testflutter/MainActivity.java
+++ app/src/main/java/com/sinosoft/testflutter/MainActivity.java
@@ -1,7 +1,10 @@
 package com.sinosoft.testflutter;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //hahah
+        Button open = findViewById(R.id.openBtn);
+        open.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, MyFlutterActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
